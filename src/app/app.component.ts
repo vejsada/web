1d091ea735eaef08b404653c39a9543ff7875595
Vejsada Jan <jan.vejsada@mestokm.cz>
2024-05-06 13:14:37 +0200

    ‘prvni’

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GovDesignSystemModule } from "@gov-design-system-ce/angular";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  
 })
 export class AppComponent {
   title = 'test-app';
